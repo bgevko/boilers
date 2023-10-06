057d25c70b3e90255993e51b9f8a73d62d977654
bgevko <bgevko@gmail.com>
2023-10-06 13:48:06 +0300

    Remove react template, add index tailwind template

--- boilers.py
+++ boilers.py
@@ -40,7 +40,7 @@ def main():
     cppcmake = subparsers.add_parser('cmake-cpp', help='CMake boilerplate for C++.')   # noqa: F841
     buildscript = subparsers.add_parser('buildscript', help='Template for a build bash script')   # noqa: F841
     express = subparsers.add_parser('express', help='Template for an express server')   # noqa: F841
-    simple_react = subparsers.add_parser('simple-react', help='Create a minimal React project with just two pages, index.html and app.jsx')   # noqa: F841
+    tailwind_html = subparsers.add_parser('tailwind-html', help='Blank index.html with tailwind cdn.')   # noqa: F841
 
 
     args = vars(parser.parse_args())
@@ -63,13 +63,6 @@ def main():
         console.print('Fetching build script template...', style="yellow")
         content = get_template('https://raw.githubusercontent.com/bgevko/boilers/main/templates/rebuild.sh')
         write_file('build.sh', content)
-    elif args['command'] == 'simple-react':
-        console.print('Fetching HTML file...', style="yellow")
-        content = get_template('https://raw.githubusercontent.com/bgevko/boilers/main/templates/simple-react.html')
-        write_file('index.html', content)
-        console.print('Fetching app.jsx...', style="yellow")
-        content = get_template('https://raw.githubusercontent.com/bgevko/boilers/main/templates/simple-react-app.jsx')
-        write_file('app.jsx', content)
     elif args['command'] == 'express':
         console.print('Fetching express server template...', style="yellow")
         content = get_template('https://raw.githubusercontent.com/bgevko/boilers/main/templates/express.js')
@@ -78,6 +71,10 @@ def main():
         content = get_template('https://raw.githubusercontent.com/bgevko/boilers/main/templates/express-package.json')
         write_file('package.json', content)
         console.print("Run 'npm install' to install dependencies.", style="yellow")
+    elif args['command'] == 'tailwind-html':
+        console.print('Fetching tailwind index.html template...', style="yellow")
+        content = get_template('https://raw.githubusercontent.com/bgevko/boilers/main/templates/tailwind-index.html')
+        write_file('index.html', content)
     else:
         parser.print_help()
 if __name__ == "__main__":
